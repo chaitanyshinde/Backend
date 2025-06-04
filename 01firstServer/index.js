ae0a76fc50fbf5591315bf0b40cc47d5e95f0b65
user.name "chaitanyshinde2323@gmail.com <chaitanyshinde2323@gmail.com>
2025-06-04 22:52:50 +0530

    first server

--- 01firstServer/index.js
+++ 01firstServer/index.js
@@ -1,12 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 const port = 3000;
 
+const data = {
+  name: "Chaitany",
+  age: 22,
+  city: "pune",
+  edu: "BE EnTC",
+  college: "SKNCOE",
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/login", (req, res) => {
+  res.send("<h1>We are on login page!</h1>");
+});
 
-app.listen(port, () => {
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
