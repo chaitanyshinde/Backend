648071bd84ce752c5636b9f9649db3e869f003d5
user.name "chaitanyshinde2323@gmail.com <chaitanyshinde2323@gmail.com>
2025-06-21 22:31:09 +0530

    orderItem Schema

--- 02DataModling/models/ecommerce/order.models.js
+++ 02DataModling/models/ecommerce/order.models.js
@@ -1,5 +1,32 @@
 import mongoose from "mongoose";
 
-const orderSchema = mongoose.Schema({}, { timestamps: true });
+const orderItemSchema = mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: {
+      type: [orderItemSchema],
+    },
+  },
+
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", orderSchema);
